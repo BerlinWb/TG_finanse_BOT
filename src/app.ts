839cd8c3b79ecdb0fc1f0e5b9884cfceb7dd795e
BerlinWb <hermond8@icloud.com>
2022-10-19 18:48:40 +0300

    chore(project): Configure simple chat example.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.info("Hello world!")
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf("5579281478:AAE7F2u1cq9TgYPGPr8tYR69vYEKe66XZTk");
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there!!!'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
